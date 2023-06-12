3a185f6e0aaf7019d9a12bff0be358b94920d98c
Jens Teichert <git@jensteichert.de>
2023-06-12 22:49:14 +0200

    Update collection_test.go

--- collection_test.go
+++ collection_test.go
@@ -100,16 +100,65 @@ func TestCollection_Find_Empty(t *testing.T) {
 	mockDb.Disconnect()
 }
 
+/*func TestCollection_Find_WithError(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	collection := GetCollection[*testdoc](&mockDb, "testdocs")
 
+	result, err := collection.Find(bson.M{"title": "NonExisting"})
 
-// TODO
+	assert.NotNil(t, err)
+	assert.Nil(t, result)
+}*/
+
 func TestCollection_UpdateOne(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	mockDb.Connect("mongodb://localhost:27017/colt?readPreference=primary&directConnection=true&ssl=false", "colt")
 
+	collection := GetCollection[*testdoc](&mockDb, "testdocs")
+
+	random := fmt.Sprint(rand.Int())
+	doc := testdoc{Title: random}
+
+	collection.Insert(&doc)
+
+	fmt.Println(doc.ID)
+	doc.Title = doc.Title + " updated"
+	err := collection.UpdateOne(bson.M{"title": random}, &doc)
+
+	totalWithNewTitle, err := collection.CountDocuments(bson.M{"title": random + " updated"})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, totalWithNewTitle)
+	assert.Equal(t, totalWithNewTitle, int64(1))
+
+	mockDb.Disconnect()
 }
 
-// TODO
 func TestCollection_UpdateMany(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	mockDb.Connect("mongodb://localhost:27017/colt?readPreference=primary&directConnection=true&ssl=false", "colt")
 
+	collection := GetCollection[*testdoc](&mockDb, "testdocs")
+
+	random := fmt.Sprint(rand.Int())
+	doc := testdoc{Title: random}
+	doc2 := testdoc{Title: random}
+
+	collection.Insert(&doc)
+	collection.Insert(&doc2)
+
+	err := collection.UpdateMany(bson.M{"title": doc.Title}, bson.M{"$set": bson.M{"title": doc.Title + " updated"}})
+
+	fmt.Println(err)
+	time.Sleep(200 * time.Millisecond)
+
+	totalWithNewTitle, err := collection.CountDocuments(bson.M{"title": doc.Title + " updated"})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, totalWithNewTitle)
+	assert.Equal(t, totalWithNewTitle, int64(2))
+
+	mockDb.Disconnect()
 }
 
 // TODO
