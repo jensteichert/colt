8aa53c0f08d3dce6a8a44d1be6ad0bff8b3f33f4
Jens Teichert <git@jensteichert.de>
2024-03-24 13:10:06 +0100

    add support for multi key indices

--- indexes.go
+++ indexes.go
@@ -5,9 +5,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (repo *Collection[T]) CreateIndex(keys bson.M)  error {
+func (repo *Collection[T]) CreateIndex(keys bson.D)  error {
 	mod := mongo.IndexModel{
-		Keys: keys, Options: nil,
+		Keys: keys,
+		Options: nil,
 	}
 	_, err := repo.collection.Indexes().CreateOne(DefaultContext(), mod)
 
