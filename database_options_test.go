02bd7ba3ebf4c1de025c2e7883f02c5a473bbd9e
Jakob Lorz <jakoblorz@users.noreply.github.com>
2024-11-27 21:49:09 +0100

    fix: add coverage for the default logger

--- database_options_test.go
+++ database_options_test.go
@@ -1,6 +1,7 @@
 package colt
 
 import (
+	"log"
 	"reflect"
 	"testing"
 )
@@ -26,6 +27,15 @@ func TestWithLogger(t *testing.T) {
 }
 
 func Test_databaseOptions_Logger(t *testing.T) {
+	t.Run("should return the default logger", func(t *testing.T) {
+		want := log.Default()
+		db := &Database{options: databaseOptions{}}
+
+		if got := db.options.Logger(); !reflect.DeepEqual(want, got) {
+			t.Errorf("WithLogger() = %v, want %v", got, want)
+		}
+	})
+
 	t.Run("should return the provided logger from the database", func(t *testing.T) {
 		want := &testLogger{}
 		db := &Database{options: databaseOptions{
